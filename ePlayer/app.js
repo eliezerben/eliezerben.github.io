f3a93712d14a17b3ce9be35ece7f522f914c9b27
Eliezer Benjamin <eliezerben96@gmail.com>
2023-08-18 15:53:48 +0530

    Update style of demo page

--- ePlayer/app.js
+++ ePlayer/app.js
@@ -1,25 +1,25 @@
 var playlist_1 = [
     { 
-        url: "https://eliezerben.github.io/music/bensound-slowmotion.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music1.mp3",
         name: "Slow Motion"        
     },{
-        url: "https://eliezerben.github.io/music/bensound-tenderness.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music2.mp3",
         name: "Tenderness"
     },{
-        url: "https://eliezerben.github.io/music/bensound-onceagain.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music3.mp3",
         name: "Once Again"
     },{
-        url: "https://eliezerben.github.io/music/bensound-acousticbreeze.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music4.mp3",
         name: "Acoustic Breeze"
     },{
-        url: "https://eliezerben.github.io/music/bensound-sunny.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music5.mp3",
         name: "Sunny"
     }
 ];
 var options_1 = {
     playlist: true,
     repeat: 'on',
-    shuffle: 'off',
+    shuffle: 'on',
     volume: 0.7,
     autoplay: false,
     preload: 'auto'
@@ -29,19 +29,19 @@ var p1 = ePlayer.darkTheme('p1', options_1, playlist_1);
 
 var playlist_2 = [
     { 
-        url: "https://eliezerben.github.io/music/bensound-slowmotion.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music1.mp3",
         name: "Slow Motion"        
     },{
-        url: "https://eliezerben.github.io/music/bensound-tenderness.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music2.mp3",
         name: "Tenderness"
     },{
-        url: "https://eliezerben.github.io/music/bensound-onceagain.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music3.mp3",
         name: "Once Again"
     },{
-        url: "https://eliezerben.github.io/music/bensound-acousticbreeze.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music4.mp3",
         name: "Acoustic Breeze"
     },{
-        url: "https://eliezerben.github.io/music/bensound-sunny.mp3",
+        url: "https://eliezerben.github.io/ePlayer/music/music5.mp3",
         name: "Sunny"
     }
 ];
